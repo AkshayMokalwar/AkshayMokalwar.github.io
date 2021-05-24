f42ca8a1c2c5dde140e29c80dc9e475d39f78ac1
Akshay Mokalwar <aamokalwar@gmail.com>
2021-05-25 00:42:24 +0530

    new

--- JustAsk/qna/urls.py
+++ JustAsk/qna/urls.py
@@ -5,10 +5,10 @@ from qna import views
 
 urlpatterns = [
 
-	path('home/',views.homeakview),
-	#path('home/',views.home_view),
-	#path('ques/<int:qid>/',views.ques_view),
-	#path('registration/',views.registration_view)
+	# path('home/',views.homeakview),
+	path('home/',views.home_view),
+	path('ques/<int:qid>/',views.ques_view),
+	path('registration/',views.registration_view)
 
 
 ]
