994e4771b2b74e6613048a25dbbcae6498122364
Akshay Mokalwar <aamokalwar@gmail.com>
2021-05-25 01:06:01 +0530

    final

--- JustAsk/JustAsk/settings.py
+++ JustAsk/JustAsk/settings.py
@@ -15,12 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIR= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    STATIC_DIR,
 )
 
 # Quick-start development settings - unsuitable for production
