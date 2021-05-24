8d6a03dd6181b9a87d21d9fc53cd991df94f492f
Akshay Mokalwar <aamokalwar@gmail.com>
2021-05-25 01:21:07 +0530

    dis

--- JustAsk/JustAsk/settings.py
+++ JustAsk/JustAsk/settings.py
@@ -22,7 +22,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+DISABLE_COLLECTSTATIC=1
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
