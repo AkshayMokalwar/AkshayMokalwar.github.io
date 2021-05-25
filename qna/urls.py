63b3018b686f1f90de709b4ed7a31bc1428c6f49
Akshay Mokalwar <aamokalwar@gmail.com>
2021-05-25 23:30:57 +0530

    minor

--- qna/urls.py
+++ qna/urls.py
@@ -5,7 +5,7 @@ from qna import views
 
 urlpatterns = [
 
-	path('home/',views.homeakview),
+	path('',views.homeakview),
 	# path('home/',views.home_view),
 	# path('ques/<int:qid>/',views.ques_view),
 	# path('registration/',views.registration_view)
